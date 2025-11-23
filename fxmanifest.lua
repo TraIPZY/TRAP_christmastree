a173d483d01632824dda461a030042235e992371
Trap <148614470+TraIPZY@users.noreply.github.com>
2025-11-23 10:17:53 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'TRAP_christmastree'
+author 'TRAPZY'
+description 'Christmas Tree Daily Gift System (ESX + ox_inventory)'
+
+
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
+
+dependencies {
+    'es_extended',
+    'ox_inventory',
+    'ox_target',
+    'ox_lib'
+}

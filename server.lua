2f904fe0e7b6535b3ae15c53ec39072936829a77
Trap <148614470+TraIPZY@users.noreply.github.com>
2025-12-12 23:06:46 -0500

    Update server.lua

--- server.lua
+++ server.lua
@@ -53,3 +53,19 @@ RegisterNetEvent("TRAP_christmastree:claimGift", function()
         end
     )
 end)
+
+AddEventHandler('onResourceStart', function(resourceName)
+    if resourceName ~= GetCurrentResourceName() then return end
+
+    print('^4████████╗██████╗  █████╗ ██████╗ ')
+    print('^4╚══██╔══╝██╔══██╗██╔══██╗██╔══██╗')
+    print('^4   ██║   ██████╔╝███████║██████╔╝')
+    print('^4   ██║   ██╔══██╗██╔══██║██╔═══╝ ')
+    print('^4   ██║   ██║  ██║██║  ██║██║     ')
+    print('^4   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ')
+    print('^7')
+    print('^6   Discord : https://discord.gg/rjjU2y93X7')
+    print('^8   Créé par TRAPZY')
+    print('^3   © TRAP Development')
+
+end)
